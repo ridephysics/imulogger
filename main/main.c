e84ebf5366efb2e80231a0694a59b6fae33ef3a6
M1cha <sigmaepsilon92@gmail.com>
2019-06-01 06:17:13 +0200

    main: init uev

--- main/main.c
+++ main/main.c
@@ -1,7 +1,28 @@
+#include <uev/uev.h>
+
 #define CROSSLOG_TAG "main"
 #include <crosslog.h>
 
+static uev_ctx_t _uev;
+static uev_ctx_t * const uev = &_uev;
+
 void app_main(void)
 {
+    int rc;
+
     CROSSLOGI("Hello world!");
+
+    rc = uev_init(uev);
+    if (rc) {
+        CROSSLOGE("can't init uev");
+        CROSSLOG_ASSERT(0);
+    }
+
+    rc = uev_run(uev, 0);
+    if (rc) {
+        CROSSLOGE("uev_run returned %d", rc);
+    }
+
+    uev_exit(uev);
+    CROSSLOGD("main task finished");
 }
